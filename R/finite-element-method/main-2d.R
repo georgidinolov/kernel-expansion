68788c8622b07a12a3e52d51ca6e5732763a91e6
Georgi Svetoslavov Dinolov <gdinolov@grape.soe.ucsc.edu>
2017-02-23 19:27:13 -0800

    running on grape

--- R/finite-element-method/main-2d.R
+++ R/finite-element-method/main-2d.R
@@ -21,71 +21,61 @@ dt <- problem.parameters.generate.data$t/1000;
 n.samples <- 100;
 
 data <- load.data.from.csv(
-    "~/research/PDE-solvers/data/data-set-1.csv");
+    "~/PDE-solvers/data/data-set-1.csv");
 
-data[[1]]$ax = -1.56035;
-data[[1]]$x.fc = -0.684587;
-data[[1]]$bx = 0.0261297;
-data[[1]]$x.ic = 0;
+## data.files.list <- list.files(path = "~/research/PDE-solvers/data",
+##                              pattern = "data-set-*", full.names = TRUE);
+## print(data.files.list);
 
-data[[1]]$ay = -0.871711;
-data[[1]]$y.fc = -0.154531;
-data[[1]]$by = 0.126382;
-data[[1]]$y.ic = 0;
+## cl <- makeCluster(4);
+## estimates.mle <- parLapply(cl=cl, X=data.files.list,
+##                            function(x) {
+##                                source("2-d-solution.R");
+##                                library("mvtnorm");
+##                                data <- load.data.from.csv(x);
+##                                mles <- mle.estimator.no.boundary(data, 1, 1, 0.0);
+##                                return(mles);
+##                            });
+## stopCluster(cl);
 
-data.files.list <- list.files(path = "~/research/PDE-solvers/data",
-                             pattern = "data-set-*", full.names = TRUE);
-print(data.files.list);
+## estimates.mle.rhos <- rep(NA, length(estimates.mle));
+## for (i in seq(1,length(estimates.mle))) {
+##     estimates.mle.rhos[i] = estimates.mle[[i]]$rho.mle;
+## }
+## sqrt(mean((estimates.mle.rhos-0.8)^2));
 
-cl <- makeCluster(4);
-estimates.mle <- parLapply(cl=cl, X=data.files.list,
-                           function(x) {
-                               source("2-d-solution.R");
-                               library("mvtnorm");
-                               data <- load.data.from.csv(x);
-                               mles <- mle.estimator.no.boundary(data, 1, 1, 0.0);
-                               return(mles);
-                           });
-stopCluster(cl);
+## results.files.list <- list.files(path = "~/research/PDE-solvers/data",
+##                               pattern = "order-64-rel-tol", full.names = TRUE);
+## print(results.files.list);
+## estimates.fd <- rep(NA,length(results.files.list));
+## for (i in seq(1,length(results.files.list))) {
+##     mles <- load.results.from.csv(results.files.list[i]);
+##     if(!is.null(mles)) {
+##         estimates.fd[i] <- mles$rho;
+##     }
+## }
+## sqrt(mean((estimates.fd[!is.na(estimates.fd)]-0.8)^2));
 
-estimates.mle.rhos <- rep(NA, length(estimates.mle));
-for (i in seq(1,length(estimates.mle))) {
-    estimates.mle.rhos[i] = estimates.mle[[i]]$rho.mle;
-}
-sqrt(mean((estimates.mle.rhos-0.8)^2));
+## estimates.rogers <- estimator.rodgers(data.files.list, 0.8);
 
-results.files.list <- list.files(path = "~/research/PDE-solvers/data",
-                              pattern = "order-64-rel-tol", full.names = TRUE);
-print(results.files.list);
-estimates.fd <- rep(NA,length(results.files.list));
-for (i in seq(1,length(results.files.list))) {
-    mles <- load.results.from.csv(results.files.list[i]);
-    if(!is.null(mles)) {
-        estimates.fd[i] <- mles$rho;
-    }
-}
-sqrt(mean((estimates.fd[!is.na(estimates.fd)]-0.8)^2));
+## y.lims <- c(0,
+##             max(max(density(estimates.fd[!is.na(estimates.fd)])$y),
+##                 max(density(estimates.mle.rhos)$y),
+##                 max(density(estimates.rogers)$y)));
 
-estimates.rogers <- estimator.rodgers(data.files.list, 0.8);
+## x.lims <- c(min(min(density(estimates.fd[!is.na(estimates.fd)])$x),
+##                 min(density(estimates.mle.rhos)$x),
+##                 min(density(estimates.rogers)$x)),
+##             max(max(density(estimates.fd[!is.na(estimates.fd)])$x),
+##                 max(density(estimates.mle.rhos)$x),
+##                 max(density(estimates.rogers)$x)));
 
-y.lims <- c(0,
-            max(max(density(estimates.fd[!is.na(estimates.fd)])$y),
-                max(density(estimates.mle.rhos)$y),
-                max(density(estimates.rogers)$y)));
-
-x.lims <- c(min(min(density(estimates.fd[!is.na(estimates.fd)])$x),
-                min(density(estimates.mle.rhos)$x),
-                min(density(estimates.rogers)$x)),
-            max(max(density(estimates.fd[!is.na(estimates.fd)])$x),
-                max(density(estimates.mle.rhos)$x),
-                max(density(estimates.rogers)$x)));
-
-plot(density(estimates.fd[!is.na(estimates.fd)]),
-     ylim=y.lims,
-     xlim=x.lims);
-lines(density(estimates.mle.rhos),col="red");
-lines(density(estimates.rogers),col="green");
-abline(v=rho.true, col="red", lwd=2);
+## plot(density(estimates.fd[!is.na(estimates.fd)]),
+##      ylim=y.lims,
+##      xlim=x.lims);
+## lines(density(estimates.mle.rhos),col="red");
+## lines(density(estimates.rogers),col="green");
+## abline(v=rho.true, col="red", lwd=2);
 
 problem.parameters <- data[[1]];
 problem.parameters$K.prime <- K.prime;
@@ -97,50 +87,7 @@ function.list <- vector("list", K);
 x <- seq(0,1,by=dx);
 y <- seq(0,1,by=dy);
 
-## alpha.minus.1.xs <- rep(NA,K);
-## alpha.minus.1.ys <- rep(NA,K);
-## for (k in seq(1,K)) {
-##     alpha.minus.1.y <- ceiling(k/(K.prime-1));
-##     alpha.minus.1.x <- k - ((K.prime-1) * (alpha.minus.1.y-1));
-    
-##     alpha.minus.1.ys[k] <- alpha.minus.1.y;
-##     alpha.minus.1.xs[k] <- alpha.minus.1.x;
-## }
-## alpha.xs <- alpha.minus.1.xs + 1;
-## alpha.ys <- alpha.minus.1.ys + 1;
-## ## alpha + beta = K.prime + 2
-
-## x.basis.coors <- alpha.minus.1.xs / (K.prime);
-## y.basis.coors <- alpha.minus.1.ys / (K.prime);
-
-## sort.bases <- sort.int(sqrt((x.basis.coors-0.5)^2 +
-##                             (y.basis.coors-0.5)^2),
-##                        index.return = TRUE);
-## alpha.xs <- alpha.xs[sort.bases$ix];
-## alpha.ys <- alpha.ys[sort.bases$ix];
-
-## for (k in seq(1,K)) {
-##     alpha.x <- alpha.xs[k];
-##     alpha.y <- alpha.ys[k];
-##     print(c(alpha.x,alpha.y));
-##     function.params <- c(alpha.x, alpha.y);
-    
-##     function.list[[k]] <-
-##         basis.function.xy(x,y,
-##                           function.params,
-##                           problem.parameters);
-## }
-
-## par(mfrow=c(ceiling(sqrt(K)),
-##             ceiling(sqrt(K))));
-## par(mar = c(5,4,2,1));
-## if (PLOT.SOLUTION) {
-##     for (k in seq(1,K)) {
-##         contour(x,y,function.list[[k]]);
-##     }
-## }
-
-sigma=0.3;
+sigma=0.20;
 sigma2=sigma^2;
 l=1;
 function.list <-
@@ -160,24 +107,30 @@ for (n in seq(1,length(data))) {
     problem.parameters.original <- data[[n]];
     problem.parameters.original$K.prime <- K.prime;
     problem.parameters.original$number.terms <- 100;
+
     problem.parameters.original$sigma.2.x <-
         problem.parameters.generate.data$sigma.2.x;
+
     problem.parameters.original$sigma.2.y <-
         problem.parameters.generate.data$sigma.2.y;
-    problem.parameters.original$rho <-
-        problem.parameters.generate.data$rho;
+
+    problem.parameters.original$rho <- -0.80;
+
+    ## problem.parameters.original$rho <-
+    ##     problem.parameters.generate.data$rho;
+
     l2.1 <- blackbox(function.list,
                      orthonormal.function.list,
                      system.mats,
                      problem.parameters.original,
                      dx,dy,
-                     TRUE, FALSE);
+                     FALSE, FALSE);
     
-    print (l2.1 *
+    print(c(l2.1, l2.1 *
            1.0/((problem.parameters.original$bx-
-                 problem.parameters.original$ax) *
+                 problem.parameters.original$ax)^3 *
                 (problem.parameters.original$by-
-                 problem.parameters.original$ay)));
+                 problem.parameters.original$ay)^3)));
     
     ## problem.parameters.original$ax <- problem.parameters.original$ax - dx;
     ## l2.2 <- blackbox(function.list,
@@ -189,7 +142,7 @@ for (n in seq(1,length(data))) {
 
     ## -(l2.1-l2.2)/dx;
 
-    print(paste("n=", n, "; l2.1 = ", l2.1));   
+    ## print(paste("n=", n, "; l2.1 = ", l2.1));   
     cat("Press [ENTER] to continue");
     line <- readline();
 }
