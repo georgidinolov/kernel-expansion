5f5122f7f9c00aa30c60b021c5a84616441a88d4
Georgi Dinolov <gdinolov@soe.ucsc.edu>
2019-03-12 09:43:02 -0700

    final drafts

--- documentation/chapter-2/plot-results.R
+++ documentation/chapter-2/plot-results.R
@@ -39,26 +39,28 @@ rho.hist = ggplot(all.est, aes(y=..density.., x=rho, fill=type)) +
     geom_density(alpha=0.5) +
     xlab(expression(rho)) +
     labs(title=paste0("m=", data_size)) +
-    geom_vline(xintercept=rho_data)
+    geom_vline(xintercept=rho_data) +
+    theme(legend.position="none")
 
 sigma.x.hist = ggplot(all.est, aes(y=..density.., x=sigma_x, fill=type)) +
     geom_density(alpha=0.5) +
+    ylab(NULL) +
     xlab(expression(sigma[x])) +
     labs(title=paste0("m=", data_size)) +
-    geom_vline(xintercept=1)
+    geom_vline(xintercept=1) +
+    theme(legend.position="none")
 
 sigma.y.hist = ggplot(all.est, aes(y=..density.., x=sigma_y, fill=type)) +
     geom_density(alpha=0.5) +
     xlab(expression(sigma[y])) +
+    ylab(NULL) +
     labs(title=paste0("m=", data_size)) +
-    geom_vline(xintercept=1)
+    geom_vline(xintercept=1) +
+    theme(legend.position="none")
 
 all = grid.arrange(rho.hist, sigma.x.hist, sigma.y.hist, nrow=1)
 
-ggsave(filename=paste0(save.path, "estimates-rho.pdf"), plot=rho.hist, width=6, height=6, units="in")
-ggsave(filename=paste0(save.path, "estimates-sigma-x.pdf"), plot=sigma.x.hist, width=6, height=6, units="in")
-ggsave(filename=paste0(save.path, "estimates-sigma-y.pdf"), plot=sigma.y.hist, width=6, height=6, units="in")
-ggsave(filename=paste0(save.path, "estimates-all.pdf"), plot=all, width = 20, height=6, units = "in")
-
-
-
+ggsave(filename=paste0(save.path, "estimates-rho.pdf"), plot=rho.hist, width=2, height=2, units="in")
+ggsave(filename=paste0(save.path, "estimates-sigma-x.pdf"), plot=sigma.x.hist, width=2, height=2, units="in")
+ggsave(filename=paste0(save.path, "estimates-sigma-y.pdf"), plot=sigma.y.hist, width=2, height=2, units="in")
+ggsave(filename=paste0(save.path, "estimates-all.pdf"), plot=all, width = 8, height=6, units = "in")
